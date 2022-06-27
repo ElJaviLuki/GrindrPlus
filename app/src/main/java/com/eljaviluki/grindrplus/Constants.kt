846bb4d6bd3edfaaf9d69a19f91743bba22c7342
ElJaviLuki <eljaviluki@gmail.com>
2022-06-27 12:03:47 +0200

    Explicit types in return constants
    The declarations of such constants have type inferred from a platform call, which can lead to unchecked nullability issues, so we specify type explicitly as  non-nullable.

--- app/src/main/java/com/eljaviluki/grindrplus/Constants.kt
+++ app/src/main/java/com/eljaviluki/grindrplus/Constants.kt
@@ -6,9 +6,9 @@ object Constants {
     const val GRINDR_PKG = "com.grindrapp.android"
 
     object Returns {
-        val RETURN_TRUE = XC_MethodReplacement.returnConstant(true)
-        val RETURN_FALSE = XC_MethodReplacement.returnConstant(false)
-        val RETURN_INTEGER_MAX_VALUE = XC_MethodReplacement.returnConstant(Int.MAX_VALUE)
-        val RETURN_LONG_MAX_VALUE = XC_MethodReplacement.returnConstant(Long.MAX_VALUE)
+        val RETURN_TRUE: XC_MethodReplacement = XC_MethodReplacement.returnConstant(true)
+        val RETURN_FALSE: XC_MethodReplacement = XC_MethodReplacement.returnConstant(false)
+        val RETURN_INTEGER_MAX_VALUE: XC_MethodReplacement = XC_MethodReplacement.returnConstant(Int.MAX_VALUE)
+        val RETURN_LONG_MAX_VALUE: XC_MethodReplacement = XC_MethodReplacement.returnConstant(Long.MAX_VALUE)
     }
 }
