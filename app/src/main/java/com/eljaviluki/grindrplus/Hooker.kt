faadf4d163c0c6b8f335b5970c85bc9da371bf85
ElJaviLuki <eljaviluki@gmail.com>
2022-09-18 15:02:42 +0200

    Bump to 8.17.1

--- app/src/main/java/com/eljaviluki/grindrplus/Hooker.kt
+++ app/src/main/java/com/eljaviluki/grindrplus/Hooker.kt
@@ -108,7 +108,7 @@ class Hooker : IXposedHookLoadPackage {
     }
 
     companion object {
-        const val TARGET_PKG_VERSION_NAME = "8.17.0"
+        const val TARGET_PKG_VERSION_NAME = "8.17.1"
 
         var pkgParam: LoadPackageParam by InitOnce()
         var appContext: Context by InitOnce()
