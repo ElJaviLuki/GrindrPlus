daedd173ae8d4b4c191a706fd19b8dea8146aa46
Poundr <poundross@gmail.com>
2024-04-26 00:53:16 +0200

    UnlimitedAlbums: Speed up subsequent calls to getContent

--- app/src/main/java/com/grindrplus/hooks/UnlimitedAlbums.kt
+++ app/src/main/java/com/grindrplus/hooks/UnlimitedAlbums.kt
@@ -4,12 +4,18 @@ import com.grindrplus.utils.Hook
 import com.grindrplus.utils.HookStage
 import com.grindrplus.utils.hook
 import de.robv.android.xposed.XposedHelpers.getObjectField
-import de.robv.android.xposed.XposedHelpers.setObjectField
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 class UnlimitedAlbums: Hook("Unlimited albums",
     "Allow to be able to view unlimited albums") {
     private val albumModel = "com.grindrapp.android.model.Album"
     private val albumContent = "com.grindrapp.android.model.AlbumContent"
+
+    private val scope = CoroutineScope(Dispatchers.IO)
+    private val albumCache = mutableMapOf<Any, MutableMap<String, Any>>()
 
     override fun init() {
         val albumModelClass = findClass(albumModel)
@@ -24,25 +30,32 @@ class UnlimitedAlbums: Hook("Unlimited albums",
                 param.setResult(true)
             }
 
-        albumModelClass?.hook(
-            "getContent", HookStage.AFTER) { param ->
-                val contentList = param.getResult() as List<*>
-                val albumId = getObjectField(param.thisObject(), "albumId") as Long
+        albumModelClass?.hook("getContent", HookStage.AFTER) { param ->
+            /**
+             * If we've already processed this object, return the cached value
+             */
+            val cachedContent = albumCache[param.thisObject()]?.get("content")
+            if (cachedContent != null) {
+                param.setResult(cachedContent)
+                return@hook
+            }
 
-                /**
-                 * The first step is to find out if we're dealing with cover
-                 * images or not. If we are, we don't have to do anything.
-                 */
-                if (contentList.isNotEmpty()) {
-                    /**
-                     * Covers usually have only one image, so we can just
-                     * detect if it's a cover or not by checking the size of
-                     * the list.
-                     */
-                    if (contentList.size == 1) {
-                        return@hook // We're dealing with a cover image
-                    }
+            val contentList = param.getResult() as List<*>
 
+            /**
+             * The first step is to find out if we're dealing with cover
+             * images or not. If we are, we don't have to do anything.
+             *
+             * Covers usually have only one image, so we can just
+             * detect if it's a cover or not by checking the size of
+             * the list.
+             */
+            if (contentList.size == 1) return@hook
+
+            val albumId = getObjectField(param.thisObject(), "albumId") as Long
+            if (contentList.isNotEmpty()) {
+                // TODO: Check for race conditions
+                scope.launch {
                     /**
                      * At this point, we know we're dealing with an actual
                      * album. We can now proceed to save the contents if
@@ -57,36 +70,45 @@ class UnlimitedAlbums: Hook("Unlimited albums",
                         deleteContentsByList(albumId)
                     }
                     saveContentsByList(albumId, contentList)
-                } else {
-                    /**
-                     * If the content is null, it means that the album is
-                     * empty and we could try to retrieve the contents again.
-                     */
-                    if (context.database.getContentIdListByAlbumId(albumId).isNotEmpty()) {
-                        val newContentsList = mutableListOf<Any>()
-                        val savedContentsList = context.database.getContentIdListByAlbumId(albumId)
-                        for (element in savedContentsList) {
-                            val savedContent = context.database.getAlbumContent(element)
-                            if (savedContent != null) {
-                                loadClass(albumContent)?.constructors?.first()?.newInstance(
-                                    savedContent.getAsLong("contentId"),
-                                    savedContent.getAsString("contentType"),
-                                    savedContent.getAsString("url"),
-                                    savedContent.getAsBoolean("isProcessing"),
-                                    savedContent.getAsString("thumbUrl"),
-                                    savedContent.getAsString("coverUrl"),
-                                    savedContent.getAsInteger("remainingViews")
-                                )?.let { newContentsList.add(it) }
-                            }
+                }
+            } else {
+                /**
+                 * If the content list is empty, try to retrieve the contents
+                 * from the database and set them as the new content list.
+                 */
+                val savedContentsList = context.database.getContentIdListByAlbumId(albumId)
+                if (savedContentsList.isNotEmpty()) {
+                    val newContentsList = mutableListOf<Any>()
+
+                    for (element in savedContentsList) {
+                        val savedContent = context.database.getAlbumContent(element)
+                        if (savedContent != null) {
+                            loadClass(albumContent)?.constructors?.first()?.newInstance(
+                                savedContent.getAsLong("contentId"),
+                                savedContent.getAsString("contentType"),
+                                savedContent.getAsString("url"),
+                                savedContent.getAsBoolean("isProcessing"),
+                                savedContent.getAsString("thumbUrl"),
+                                savedContent.getAsString("coverUrl"),
+                                savedContent.getAsInteger("remainingViews")
+                            )?.let { newContentsList.add(it) }
                         }
-
-                        //setObjectField(param.thisObject(), "contentCount", newContentsList.size)
-                        setObjectField(param.thisObject(), "content", newContentsList)
-
-                        param.setResult(newContentsList)
                     }
+
+                    //setObjectField(param.thisObject(), "contentCount", newContentsList.size)
+                    albumCache.getOrPut(param.thisObject()) { mutableMapOf() }["content"] =
+                        newContentsList
+
+                    // TODO: Should we also hook other getters such as "getContentCount"?
+
+                    param.setResult(newContentsList)
                 }
             }
+        }
+    }
+
+    override fun cleanup() {
+        scope.cancel()
     }
 
     private fun deleteContentsByList(albumId: Long) {
@@ -96,23 +118,21 @@ class UnlimitedAlbums: Hook("Unlimited albums",
         }
     }
 
-    private fun saveContentsByList(albumId: Long, content: Any) {
-        if (content is List<*>) {
-            for (element in content) {
-                if (getObjectField(element, "url") == null) {
-                    continue // Skip invalid / empty content
-                }
-                context.database.addAlbumContent(
-                    getObjectField(element, "contentId") as Long,
-                    albumId,
-                    getObjectField(element, "contentType") as String,
-                    getObjectField(element, "url") as String,
-                    getObjectField(element, "isProcessing") as Boolean,
-                    getObjectField(element, "thumbUrl") as String,
-                    getObjectField(element, "coverUrl") as String,
-                    getObjectField(element, "remainingViews") as Int,
-                )
+    private fun saveContentsByList(albumId: Long, content: List<*>) {
+        for (element in content) {
+            if (getObjectField(element, "url") == null) {
+                continue // Skip invalid / empty content
             }
+            context.database.addAlbumContent(
+                getObjectField(element, "contentId") as Long,
+                albumId,
+                getObjectField(element, "contentType") as String,
+                getObjectField(element, "url") as String,
+                getObjectField(element, "isProcessing") as Boolean,
+                getObjectField(element, "thumbUrl") as String,
+                getObjectField(element, "coverUrl") as String,
+                getObjectField(element, "remainingViews") as Int,
+            )
         }
     }
 
