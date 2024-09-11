a52c60cf28fbe06111483ffda62c6fa3b3434e54
rhunk <101876869+rhunk@users.noreply.github.com>
2024-09-11 15:07:10 +0200

    GrindrPlus: Improve `DisableScreenshot` hook

--- app/src/main/java/com/grindrplus/hooks/AllowScreenshots.kt
+++ app/src/main/java/com/grindrplus/hooks/AllowScreenshots.kt
@@ -1,5 +1,9 @@
 package com.grindrplus.hooks
 
+import android.app.Activity
+import android.content.ContentResolver
+import android.database.ContentObserver
+import android.net.Uri
 import android.view.Window
 import android.view.WindowManager.LayoutParams.FLAG_SECURE
 import com.grindrplus.utils.Hook
@@ -10,12 +14,24 @@ class AllowScreenshots : Hook(
     "Allow screenshots",
     "Allow screenshots everywhere in the app"
 ) {
-
     override fun init() {
         Window::class.java.hook("setFlags", HookStage.BEFORE) { param ->
             var flags = param.arg<Int>(0)
             flags = flags and FLAG_SECURE.inv()
             param.setArg(0, flags)
         }
+
+        Activity::class.java.hook("registerScreenCaptureCallback", HookStage.BEFORE) { param ->
+            param.setResult(null)
+        }
+
+        ContentResolver::class.java.methods.first {
+            it.name == "registerContentObserver" &&
+                    it.parameterTypes.contentEquals(arrayOf(android.net.Uri::class.java, Boolean::class.javaPrimitiveType, ContentObserver::class.java))
+            }.hook(HookStage.BEFORE) { param ->
+                val uri = param.arg<Uri>(0)
+                if (uri.host != "media") return@hook
+                param.setResult(null)
+            }
     }
 }
