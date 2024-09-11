4ca42c061c2f09b17f15f4ecefd323d0a79d62bf
R0rt1z2 <me@r0rt1z2.com>
2024-09-11 22:18:25 +0200

    GrindrPlus: Bump to 3.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 14
-        versionName = "3.1.5-$grindrVersion"
+        versionName = "3.2.0-$grindrVersion"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
